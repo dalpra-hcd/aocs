8a3264a04c39578bfdb5e4ad3d4f39b1ff3a662b
Hudson <dalpra.hcd@gmail.com>
2025-05-31 13:29:11 +1200

    solved: 2015: c: day01

---
+++ 2015/c/day01/aocs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define MAX_LINE_LEN 7000
+
+int main(void) {
+  char line[MAX_LINE_LEN + 1];
+
+  while (fgets(line, sizeof(line), stdin) != NULL) {
+    int i, floor;
+
+    floor = 0;
+    for (i = 0; line[i] != '\n' && line[i] != '\0'; ++i) {
+      floor += line[i] == ')' ? -1 : 1;
+
+      if (floor == -1) {
+        printf("%d\n", i + 1);
+        break;
+      }
+    }
+  }
+
+  return 0;
+}
