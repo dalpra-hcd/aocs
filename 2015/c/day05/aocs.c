88a58b160597dd433c0e82617f430028f80c890a
Hudson <dalpra.hcd@gmail.com>
2025-06-02 13:03:43 +1200

    solved: 2015: c: day05 part2

--- 2015/c/day05/aocs.c
+++ 2015/c/day05/aocs.c
@@ -4,32 +4,32 @@
 #define MAX_LINE_LEN 16
 
 static int is_nice(const char *s) {
-  size_t i;
-  int vowels_cnt, has_double;
+  int has_pair, has_repeat;
+  size_t i, j, len;
 
-  vowels_cnt = has_double = 0;
+  has_pair = has_repeat = 0;
+  len = strlen(s);
 
-  for (i = 0; s[i]; ++i) {
-    if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
-        s[i] == 'u') {
-      ++vowels_cnt;
+  for (i = 0; i + 1 < len; ++i) {
+    for (j = i + 2; j + 1 < len; ++j) {
+      if (s[i] == s[j] && s[i + 1] == s[j + 1]) {
+        has_pair = 1;
+        break;
+      }
     }
-
-    if (i > 0) {
-      if (s[i - 1] == s[i]) {
-        has_double = 1;
-      }
-
-      if ((s[i - 1] == 'a' && s[i] == 'b') ||
-          (s[i - 1] == 'c' && s[i] == 'd') ||
-          (s[i - 1] == 'p' && s[i] == 'q') ||
-          (s[i - 1] == 'x' && s[i] == 'y')) {
-        return 0;
-      }
+    if (has_pair) {
+      break;
     }
   }
 
-  return vowels_cnt >= 3 && has_double;
+  for (i = 2; i < len; ++i) {
+    if (s[i - 2] == s[i]) {
+      has_repeat = 1;
+      break;
+    }
+  }
+
+  return has_pair && has_repeat;
 }
 
 int main(void) {
@@ -38,6 +38,8 @@ int main(void) {
 
   nice_cnt = 0;
   while (fgets(line, sizeof(line), stdin)) {
+    line[strcspn(line, "\r\n")] = 0;
+
     if (is_nice(line)) {
       ++nice_cnt;
     }
